79a35aaf2eabbe44c8725eb7e9a4176cafb42ddb
LeoEricson <leoericson123@gmail.com>
2021-11-16 11:15:56 +0100

    Make base of site

---
+++ src/routes/index.svelte
@@ -0,0 +1,24 @@
+<section>
+	<div>
+		<h1><img src="/logo.svg" alt="as210805" /></h1>
+	</div>
+</section>
+
+<style lang="scss">
+	section {
+		display: flex;
+		justify-content: center;
+		align-items: flex-start;
+		min-height: 100vh;
+
+		padding: 5rem 2rem;
+
+		& > div {
+			width: 90ch;
+		}
+	}
+
+	h1 img {
+		width: 100%;
+	}
+</style>
